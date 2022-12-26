5229bfb18459bc8bae9cb30c63254155ace41285
Christopher A. Williamson <home@chrisaw.com>
2022-12-26 09:07:18 +0000

    Add boot image to unpack and repack with new kernel

--- common/scripts/3_copy_kernel.sh
+++ common/scripts/3_copy_kernel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ls -l /usr/src/kernel/out/arch/arm64/boot/
+
 cp -v \
-  /usr/src/kernel/out/arch/arm64/boot/Image.gz-dtb \
+  /usr/src/kernel/out/arch/arm64/boot/Image.gz \
   /out/

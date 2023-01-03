1ce7dadc3604af809638bfcce89d411116665e9c
Dom Rodriguez <shymega@shymega.org.uk>
2023-01-03 22:10:58 +0000

    Update script to reorder args in 1_fetch_kernel.sh
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/scripts/1_fetch_kernel.sh
+++ common/scripts/1_fetch_kernel.sh
@@ -2,13 +2,13 @@
 
 # fetch kernel sources
 git clone \
-  -b ${1:-stock-android} \
-  https://github.com/PCLineageOS-Ports/${2:-android_kernel_planet_mt6873} \
+  -b ${2:-stock-android} \
+  https://github.com/PCLineageOS-Ports/${1:-android_kernel_planet_mt6873} \
   /usr/src/kernel
 
 # git reset to specific git commit
-if [ $# -eq 2 ]; then
+if [ $# -eq 3 ]; then
   pushd /usr/src/kernel
-    git reset --hard "${2}"
+    git reset --hard "${3}"
   popd
 fi
